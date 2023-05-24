67729c8b29c97f8cbbf40a06c88d67cd0e432452
Pavlo <gerasymenko1@gmail.com>
2023-05-24 12:35:36 +0300

    without adaptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ $(function () {
   $('.slider').slick({
     arrows: false,
     fade: true,
-    autoplay: 3000
+    autoplay: 3000,
+    dots: true,
   });
+
+  $('.header-btn').on('click', function () {
+    $('.menu').addClass('active');
+  });
+
+  $('.close-btn').on('click', function () {
+    $('.menu').removeClass('active');
+  });
+
 });
